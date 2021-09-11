85daf4b3d8c35481e7441ebd2c3114d5ec1ec9f3
Bayurzx <bayurzx@gmail.com>
2021-09-11 19:48:53 +0100

    Fixed tracing error

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -27,7 +27,7 @@ from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 logger = logging.getLogger(__name__)
 handler = AzureLogHandler(
     connection_string='InstrumentationKey=c5ad5e43-4a4f-44d4-a98b-4a9c02233559')
-# handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
+handler.setFormatter(logging.Formatter('%(message)s'))
 logger.addHandler(handler)
 
 # Logging custom Events
